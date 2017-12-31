08c46ed5e0418a35ed094324970fa76251b347a6
Mackenzie Hartman <mackenzie.e.hartman@gmail.com>
2017-12-30 21:57:20 -0500

    version 1.2

--- public/js/main.js
+++ public/js/main.js
@@ -267,3 +267,16 @@ var wow = new WOW ({
 });
 wow.init();
 
+
+
+
+// ========== FAQS ========== //
+
+
+$(document).ready(function(){
+    $(".box").click(function(){
+      $(this).next().slideToggle("fast");
+      $(this).find('i').toggle();
+    });  
+
+});
